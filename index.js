16fa361c81fe4af1dfac3d9c9ec078af5a58e34f
Huy Cu <huycu17@gmail.com>
2023-01-12 16:30:52 -0500

    Add test in hello wolrd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", (req, res, next) => {
-    res.json({ message: "hello world" })
+    res.json({ message: "hello world wc" })
 })
 
 app.get("/availability", (req, res, next) => {
